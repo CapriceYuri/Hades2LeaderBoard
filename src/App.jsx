14b84380ec343556119879615d52f3e3af9a1044
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-03-18 17:36:15 -0400

    Revert changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
   return (
     <section className="h-lvh overflow-x-hidden">
       <div className="fixed bg-[#28282b] h-lvh w-full object-cover -z-10" />
-      {/* <img
+      <img
         src="mainBG.png"
         alt="cover-bg"
         className="fixed h-lvh w-full object-cover -z-10 2xl:object-top"
-      /> */}
+      />
       <NavigationBar />
       <section>
         <div className="flex justify-center px-2 mx-auto w-[100%] pt-10 pb-5 gap-x-2 2xl:w-4/5">
